3b876abce94f4736b2f77f2b1a2a2e43555bb7d1
daba clovis <dabaclovis@yahoo.com>
2026-05-29 01:06:37 -0400

    title modify

--- app/Livewire/Pages/Index.php
+++ app/Livewire/Pages/Index.php
@@ -2,20 +2,15 @@
 
 namespace App\Livewire\Pages;
 
-<<<<<<< HEAD
 
 use Livewire\Component;
 use Livewire\Attribute\Layout;
 
-=======
-use Livewire\Component;
-use Livewire\Attributes\Layout;
 
->>>>>>> ab49cd610b6031ab11d90bff061f9625509db10c
 #[Layout(
     'components.layouts.app',
     [
-        'title' => 'Home',
+        'title' => 'Home - services and offerings',
         'description' => 'Welcome to our website! Explore our services and offerings.',
         'keywords' => 'home, welcome, services, offerings',
     ]
