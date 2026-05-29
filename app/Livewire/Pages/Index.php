ce191aeb57983c93586741db2dd2cf8bdb780055
dabaclovis <dabaclovis@yahoo.com>
2026-05-29 01:08:18 -0400

    clovis

--- app/Livewire/Pages/Index.php
+++ app/Livewire/Pages/Index.php
@@ -2,16 +2,9 @@
 
 namespace App\Livewire\Pages;
 
-<<<<<<< HEAD
-
-use Livewire\Component;
-use Livewire\Attribute\Layout;
-
-=======
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 
->>>>>>> ab49cd610b6031ab11d90bff061f9625509db10c
 #[Layout(
     'components.layouts.app',
     [
