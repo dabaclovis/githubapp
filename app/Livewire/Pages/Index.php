7965218074009a2c11380dcba6828370155f52c9
daba clovis <dabaclovis@yahoo.com>
2026-05-29 00:40:16 -0400

    index page

--- app/Livewire/Pages/Index.php
+++ app/Livewire/Pages/Index.php
@@ -2,8 +2,18 @@
 
 namespace App\Livewire\Pages;
 
-use Livewire\Component;
 
+use Livewire\Component;
+use Livewire\Attribute\Layout;
+
+#[Layout(
+    'components.layouts.app',
+    [
+        'title' => 'Home',
+        'description' => 'Welcome to our website! Explore our services and offerings.',
+        'keywords' => 'home, welcome, services, offerings',
+    ]
+)]
 class Index extends Component
 {
     public function render()
