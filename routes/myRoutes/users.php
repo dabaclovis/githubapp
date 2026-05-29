73dd42eb6760e677cfcdb034613265751f4dc902
dabaclovis <dabaclovis@yahoo.com>
2026-05-29 02:25:54 -0400

    users.php modified

--- routes/myRoutes/users.php
+++ routes/myRoutes/users.php
@@ -1,11 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-use Illuminate\Support\Facades\Route;
-use App\Livewire\Services\Discussion;
 
-Route::get('/discussion', Discussion::class)->name('services.discussion');
-=======
 use App\Livewire\Services\Discussion;
 use App\Livewire\Services\Calendar;
 use Illuminate\Support\Facades\Route;
@@ -14,4 +9,4 @@ Route::get('calendar', Calendar::class)->name('services.calendar');
 Route::get('calendar/{year}/{month}', Calendar::class)->name('services.calendar.month');
 Route::get('calendar/{year}/{month}/{day}', Calendar::class)->name('services.calendar.day');
 Route::get('calendar/{year}/{month}/{day}/event/{eventId}', Calendar::class)->name('services.calendar.event');
->>>>>>> e88a0c9b0c81e2a3eb7058e616dacbbaab807268
+Route::get('/discussion', Discussion::class)->name('services.discussion');
